b3b2ee51aac90b06d0cf1f565aafa44b4da32ecd
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-06-09 17:03:20 +0300

    feat: FAQ toggle interacivity

--- scripts.js
+++ scripts.js
@@ -1,16 +1,23 @@
 // get elements from the html 
 const darkLightToggle = document.getElementById('darkModeToggle');
-const body = document.body
+const body = document.body;
+const faqQuestions = document.querySelectorAll('.faq-question');
 
 function Darklight(){
-    // when the toggle is clicked
     if(this.checked){
         body.classList.add('dark-mode')
     }
     else {
         body.classList.remove('dark-mode');
-        // localStorage.setItem('darkMode', 'disabled');
     }
 }
-
 darkLightToggle.addEventListener('change', Darklight)
+
+// FAQ logic
+function toggleFaq(){
+    this.parentElement.classList.toggle('active')
+}
+
+faqQuestions.forEach(question => {
+    question.addEventListener('click', toggleFaq);
+});
